89dc0b9596cf69220529871798ec2fd085d68249
Harini Saladi <harini@gurupanguji.com>
2013-11-23 18:19:03 -0800

    remove db symlink from deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,11 +22,6 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
 
-  desc "Symlink shared config files"
-  task :symlink_config_files do
-    run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
-  end
-
   desc "Restart applicaiton"
   task :restart do
     run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
