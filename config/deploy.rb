6331dc4f53bf46612663121530cee681ce7b8fbf
Harini Saladi <harini@gurupanguji.com>
2013-12-12 21:44:59 -0800

    testing deployment with capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,16 +7,16 @@ set :repository,  "git@github.com:harinisaladi/todoapp.git"
 #set :deploy_to, "/var/www/todoapp.com"
 set :scm, :git
 set :branch, "master"
-set :user, "ec2-user"
+set :user, "lumoid"
 set :use_sudo, false
-set :rails_env, "production"
+set :rails_env, "staging"
 set :deploy_via, :copy
-set :ssh_options, :forward_agent => true
 set :keep_releases, 3
 set :bundle_flags,    ""
 
 default_run_options[:pty] = true
-server "ec2-54-193-11-158.us-west-1.compute.amazonaws.com", :app, :web, :db, :primary => true
+server "ec2-54-201-171-168.us-west-2.compute.amazonaws.com", :app, :web, :db, :primary => true
+set :deploy_to, "/home/lumoid/todoapp"
 
 namespace :deploy do
   task :start do ; end
